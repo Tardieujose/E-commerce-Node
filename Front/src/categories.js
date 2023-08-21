0ce9b95f289f0473c047909c6d38d8a42c73459a
Tardieujose <tardieujose@gmail.com>
2023-08-20 23:28:51 -0300

    Second commit

--- Front/src/categories.js
+++ Front/src/categories.js
@@ -10,6 +10,18 @@ const categories = [
     {
         name: "Laptop",
         img: "https://cdn.computerhoy.com/sites/navi.axelspringer.es/public/media/image/2022/05/asus-rog-g513-2716601.jpg?tf=3840x"
+    },
+    {
+        name: "Technology",
+        img: "https://assets.thehansindia.com/h-upload/2021/07/31/1092805-tech.jpg?width=500&height=300"
+    },
+    {
+        name: "phone",
+        img: "https://media.gq.com.mx/photos/64b9a82164d56fe86cf2843c/master/pass/Nothing%20Phone%202.jpg"
+    },
+    {
+        name: "Laptop",
+        img: "https://cdn.computerhoy.com/sites/navi.axelspringer.es/public/media/image/2022/05/asus-rog-g513-2716601.jpg?tf=3840x"
     }
 ];
 
